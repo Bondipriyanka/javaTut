a7d5d1dbd35790a55057f55917a3aca83ca85d95
Bondipriyanka <priyamunakala5@gmail.com>
2023-12-24 20:27:16 +0530

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const accountId =123456
+let accountEmail = "sweety@gmail.com"
+var accountPasword = "09876"
+accountCity = "Vizag"
+let accountState;
+
+//accountId = 2
+accountEmail = "swwe@gmail.com"
+accountPasword = "2323435"
+accountCity = "Bengulru"
+
+
+console.log(accountId);
+ console.table([accountEmail, accountPasword, accountCity,accountState])
+// console.error("invalid input")
+ //console.info("checking data")
+ //console.assert(accountEmail==="swwe@gmail.com","info corect")
---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+//alert (3+3)//we are using node.js,not browser
+
+console.log(3+3)
+console.log("Priya")
+
+let name = "Priya"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean  =>true or false
+//null => standalone value representation of empty
+//undefined  => which is not defined
+//symbol => unique 
+//primitive data types  |
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
